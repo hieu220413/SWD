267f19ffb64e373641a35d1a8c9175d8dbddf174
hieu <nguyentrunghieu13012002@gmail.com>
2023-02-24 14:29:29 +0700

    create project spring

---
+++ estate_trading_system/src/test/java/fpt/assignment/estate_trading_system/EstateTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.assignment.estate_trading_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateTradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
